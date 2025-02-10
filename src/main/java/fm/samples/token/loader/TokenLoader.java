f6938f09d8f25d516ffe4e857f89cc4c345f75bf
Jan Sture Nielsen <jan.sture.nielsen@gmail.com>
2025-02-10 10:03:01 -0700

    Update loader.

--- src/main/java/fm/samples/token/loader/TokenLoader.java
+++ src/main/java/fm/samples/token/loader/TokenLoader.java
@@ -1,4 +1,20 @@
 package fm.samples.token.loader;
 
+import java.io.IOException;
+
 public class TokenLoader {
+    private final String url;
+    private final boolean chrome;
+
+    public TokenLoader(String url, boolean noChrome) {
+        this.url = url;
+        this.chrome = !noChrome;
+    }
+
+    public void start() throws IOException {
+        var chrome = new ProcessBuilder("google-chrome", url).start();
+
+        System.out.format("Launching %s in chrome %s%n", url, chrome);
+    }
+
 }
